d7caea3f779cee3ab40c9838728ee1db164589ec
Luis PM <luisp237@gmail.com>
2020-02-06 12:19:13 -0500

    add link back to marketing page

--- sauti-forms/src/components/SignIn.js
+++ sauti-forms/src/components/SignIn.js
@@ -16,6 +16,7 @@ const Button = styled.button`
     }
 `
 
+
 const SignIn = props => {
     const myProps = props;    
     const INITIAL_STATE = {
@@ -27,6 +28,7 @@ const SignIn = props => {
     return (
         <>
             <h1 style={{padding: '1%'}}>Sauti Studio</h1>
+            <a href="https://fervent-bassi-308b70.netlify.com/">Return to Marketing Page</a>
             <div className="styled-div">
                 <form className="styled-form" onSubmit={ handleSubmit }>
                     <label htmlFor="username">Username:</label>
